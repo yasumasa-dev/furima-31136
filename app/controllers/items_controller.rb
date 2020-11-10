ce1c398f94d630db3099ad025b7db679fdfd81f8
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-10 11:33:25 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,40 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.save 
+      redirect_to root_path 
+    else
+      render :new 
+    end
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(:title,
+                                 :image, 
+                                 :price,
+                                 :explanation,
+                                 :category_id, 
+                                 :condition_id, 
+                                 :delivery_fee_id, 
+                                 :shipment_source_id, 
+                                 :shipment_date_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+    redirect_to action: ：index
+    end
+  end
 end
