94b0025944827ef53770e06f4478ed51a5e03a13
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-10 11:33:44 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :shipment_source
+  belongs_to :shipment_date
+
   belongs_to :user
   has_one_attached :image
+  validates :title, :explanation, :price, presence: true
+
+  validates :category_id, :condition_id, :delivery_fee_id, :shipment_source_id, :shipment_date_id, numericality: { other_than: 1 } 
 end
