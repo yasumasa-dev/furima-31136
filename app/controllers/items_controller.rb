5c9ecdcbba8a7081ee5dd8381af1aae99af094a9
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-12 18:43:23 +0900

    一覧表示順の変更と表記の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @item = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
