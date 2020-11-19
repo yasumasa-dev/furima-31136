ede467f4f2fd3b3a2973fa5091b021cca44d8b36
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-19 14:38:54 +0900

    モデルのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :shipment_date
 
   belongs_to :user
+  has_one    :order
   has_one_attached :image
 
   validates :title, :explanation, :image, presence: true
