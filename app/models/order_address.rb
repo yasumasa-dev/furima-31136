6b211567d5db98d23022112dbcc40703fd184cf4
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-19 14:36:30 +0900

    モデルバリデーション設定

---
+++ app/models/order_address.rb
@@ -0,0 +1,25 @@
+class UserDonation
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :address
+    validates :building
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
+  validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number'}
+
+  def save
+    Address.create(  postal_code: postal_code,
+                   prefecture_id: prefecture_id,
+                            city: city,
+                    phone_number: phone_number,
+                        building: building,
+                        order_id: order.id)
+    Order.create(item_id: item.id, 
+                 user_id: user.id)
+  end
+end
