f665d18822c363c25752143cdf267a18806af5f3
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-20 12:04:00 +0900

    単体テストコードの記述とバリデーションの訂正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,8 +6,8 @@ class OrderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :address
-    validates :building
     validates :token
+    validates :phone_number, format: {with: /\A\d{11}\z/}
   end
 
   validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
