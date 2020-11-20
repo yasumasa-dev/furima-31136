b6e861791d1eb3b8704d51bc2b2abad587e10a29
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-20 10:11:19 +0900

    orderコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   def index
+    @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
     unless current_user.id != @item.user_id
       redirect_to root_path
@@ -11,19 +12,30 @@ class OrdersController < ApplicationController
   end
  
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(address_params)
      if @order_address.valid?
+       pay_item
        @order_address.save
-       redirect_to action: :index
+       redirect_to root_path
      else
-       render action: :new
+       render action: :index
      end
   end
  
   private
   def address_params
    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :phone_number, :building)
-                                 .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+   .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
  
+  def pay_item
+  @item = Item.find(params[:item_id])
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+  Payjp::Charge.create(
+    amount: @item.price,  # 商品の値段
+    card: address_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+  end
 end
