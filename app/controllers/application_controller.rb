4c5520a31b57c08fe6ea871511240caf5c8ee6da
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-20 16:59:12 +0900

    Basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
   def configure_permitted_parameters
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
                                                        :last_name, 
                                                        :last_name_kana,
                                                        :birth_date])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
