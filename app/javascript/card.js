0178e0fe80ee8237cbf2381721e088ae27f74bbd
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-11-20 11:35:25 +0900

    カード情報の呼出先のname修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,10 +8,10 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("number"),
-      cvc: formData.get("cvc"),
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`,
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
 
     Payjp.createToken(card, (status, response) => {
